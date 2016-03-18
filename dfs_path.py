6da5070b78627043aaf5377b0b398b5d4286f596
Debbie Ly <debbs.ly@gmail.com>
2016-03-17 23:07:10 -0400

    Added in python class to find a path between two nodes in python

---
+++ dfs_path.py
@@ -0,0 +1,11 @@
+#finding paths between two nodes in a graph
+
+def dfs_path(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
