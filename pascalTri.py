c8c4adafc16c41531e63682c14a13374ba050a44
Debbie Ly <debbs.ly@gmail.com>
2016-04-11 23:07:16 -0400

    Pascal triangle generator by level

---
+++ pascalTri.py
@@ -0,0 +1,6 @@
+def pascal(numRows):
+    p = [[1]*(i + 1) for i in range (numRows)]
+    for i in range(numRows):
+        for j in range(1, i):
+            p[i][j] = p[i-1][j-1] + p[i-1][j]
+    return p
